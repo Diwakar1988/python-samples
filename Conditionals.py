2d0de95163a898fd189738b7f58117568cb7c5e3
Diwakar Mishra <diwakarmishra.himt@gmail.com>
2017-12-30 02:07:28 +0800

    if-else and elif sample added

--- Conditionals.py
+++ Conditionals.py
@@ -1,2 +1,34 @@
-#CONDITIONAL STATEMENTS
-print("#CONDITIONAL STATEMENTS")
+'''
+REMEMBER??
+1. if-else block scope is determined by indentation/alignment not by '{}' (parenthisis) 
+2. condition results should be either 'True' or 'False' constants
+3. Any numeric value (positive/nigative), will be considered as 'True' condition
+'''
+
+num = 10
+color= "red"
+print("num=",num," color=",color)
+
+#SIMPLE IF
+print("\n#Simple if")
+if 1.5:
+    print("num is equals to 10")
+    print("with indent","num==10") #this print() is part of if block since its align to above statement
+print("without indent","num==10") #this print() is not a part of if block since its not aligned to above statement
+
+#SIMPLE IF-ELSE
+print("\n#Simple if-else")
+if num==10:
+    print("num is equals to 10")
+else:
+    print("num is no equals to 10")
+    
+#SIMPLE LIF (IF-ELSE-IF) 
+print("\n#Simple elif (if-else-if)")
+
+if color=='red':
+    print("color is RED")
+elif color=='blue':
+    print("color is BLUE")
+else:
+    print("color is not RED or BLUE")
